b9225ebf7644e0ebb4496d016540f359f254203b
Jadedinhh <mbd77@drexel.edu>
2022-05-03 19:52:27 -0400

    script

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,23 +1,24 @@
 import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+// // import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+// // import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-//register all plugins
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
+// //register all plugins
+// // gsap.registerPlugin( DrawSVGPlugin, MorphSVGPlugin);
 
-const mainTL = gsap.timeline({id:"mainTL"});
+// const mainTL = gsap.timeline({id:"mainTL"});
 
 
-function imageMorph(){
-    let tl = gsap.timeline();
+// function imageMorph(){
+//     let tl = gsap.timeline();
     
-tl.to("#Ellipse", {duration: 1, morphSVG: "#Star"}, "+=1")
-  .to("#Ellipse", {duration: 1, morphSVG: "#Circle"}, "+=1");
+// // tl.to("#Ellipse", {duration: 1, morphSVG: "#Star"}, "+=1")
+// //   .to("#Ellipse", {duration: 1, morphSVG: "#Circle"}, "+=1");
 
-  return tl;
+//   return tl;
 
-}
+// }
 
-mainTL.add(imageMorph())
-;
+// mainTL.add(imageMorph())
+// ;
+
+gsap.to("#Ellipse", {duration:1, scale: 0.5});
